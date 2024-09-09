c587e23ba58f885711637187be35a9ddbc5d385a
Cole Crown <colecrownwealthadrian@gmail.com>
2024-09-09 17:40:11 +0100

    new changes

--- app/+html.tsx
+++ app/+html.tsx
@@ -20,7 +20,7 @@ export default function Root({ children }: PropsWithChildren) {
         <ScrollViewStyleReset />
 
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
-        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
       <body>{children}</body>
@@ -28,12 +28,3 @@ export default function Root({ children }: PropsWithChildren) {
   );
 }
 
-const responsiveBackground = `
-body {
-  background-color: #fff;
-}
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #000;
-  }
-}`;
